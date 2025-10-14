a943d20f0824dd9786d9bdf01d84fff7e2dc66f1
ZZZTNAHSI <166968547+ZZZTNAHSI@users.noreply.github.com>
2025-10-14 10:58:35 -0500

    do this at home

--- trotsky/src/app/components/worldmap/WorldMap.tsx
+++ trotsky/src/app/components/worldmap/WorldMap.tsx
@@ -3,24 +3,41 @@ import "leaflet/dist/leaflet.css"
 import "leaflet-defaulticon-compatibility"
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
 import { MapContainer, TileLayer, Marker, Popup, GeoJSON  } from "react-leaflet";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
+import * as Papa from "papaparse";
+import * as L from "leaflet";
 
 
-
+type ConflictRecord = { start_date: string; end_date: string; party1_iso: string; party2_iso: string; death_toll: string; place: string; };
+const bounds = new L.LatLngBounds(
+  [-90, -180], // Southwest corner of the world
+  [90, 180]  // Northeast
+);
 
 const WorldMap: React.FC<{}> = () => {
     const [geoJsonData, setgeoJsonData] = useState(null!);
+    const [geoData, setGeoData] = useState<ConflictRecord[]>([]);
 
     useEffect(() => {
         fetch("/custom.geo.json").then((data) => data.json()).then((data) => {
             setgeoJsonData(data);
         });
-    });
+        Papa.parse<ConflictRecord>("/data.csv", {
+            header: true,
+            download: true,
+            complete: (results) => {
+                setGeoData(results.data);
+            }
+        });
+    }, []);
 
     return (
-        <MapContainer center={[0, 0]}  zoom={2} scrollWheelZoom={false} style={{ height: "750px", width: "1300px" }} >
-            {geoJsonData && <GeoJSON   data={geoJsonData}/>}
-        </MapContainer>
+        <Suspense>
+            <MapContainer maxBounds={bounds} center={[40, 0]} minZoom={2} zoom={2} scrollWheelZoom={false} style={{ height: "750px", width: "1300px", backgroundColor: "black" }} >
+                {geoJsonData && <GeoJSON   data={geoJsonData}/>}
+            </MapContainer>
+        </Suspense>
+
     )
 }
 
