9aeb04da4e3964a94b2bf8a4f19db61c92a4137a
ZZZTnahsi <Ishantmahey1@gmail.com>
2025-11-16 21:58:25 -0600

    e

--- trotsky/src/app/components/home/bottom/Texts.tsx
+++ trotsky/src/app/components/home/bottom/Texts.tsx
@@ -63,18 +63,14 @@ const Texts: React.FC<{}> = ({}) => {
         <AnimatePresence >
             {showText.text1 && <TextPara key={1} text={
                 `Yo Yo Yo Ishant up in the house. I hope you enjoy the website as much as
-                I had fun making it. This is the accumlation of all the years of self
-                studying, blood, sweat and tears into this one project. The website itself
-                is more so a momento to the skirmish that happened between Pakistan and India.
-                The place where my parents came from and where most of my family live which is
-                Jalandhar right on the border between those 2 nations. I know a lot of wars
-                don't get reported as much as they should like the plethora of wars that are
-                happening right now in Africa and some other wars which were in the media cycle
-                for a week or 2 but then left the conscious of the general public. 
+                I had fun making it. The inspiration for this website stems comes from the conflict between India and Pakistan that happened
+                earlier in 2025. Me being an Indian and having my family living on the border gave me the eureka moment needed to make this website.
+                Wars hapepening on foreign land don't last very long in the news cycle so they leave people's consciousness or they go unnoticed
+                completely. I want to show the sheer amount of human life lost due something that feels so far from us.
                 `} /> }
             {showText.text2 && <TextPara key={2} text={
                 `My goal for this website is for you to check your countries origin and see the data for yourself because I feel knowing about your
-                home country is pretty gosh darn important. I also want to highlight the various wars that are happening in the world :).
+                home country is pretty important :).
                 `}/>  }
             <AnimatePresence mode="wait" >
                 {showText.text2 && <motion.div variants={VariantsWrapper} initial="hidden" animate="visible" exit="exit">
